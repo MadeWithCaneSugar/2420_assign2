7303e85df72faad4dbeffa9f14ce7b7e737ff499
Mitchel <heyitsmemitchel@gmail.com>
2022-12-02 16:40:56 -0800

    a new commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 const fastify = require("fastify")({ logger: true })
 const fs = require("fs")
 
-fastify.get("/", async (req, rep) => {
+fastify.get("/api", async (req, rep) => {
     const stream = fs.createReadStream(("../html/index.html"))
     await rep.send(stream)
 })
+// "/etc/www/html/index.html"
 
 const start = async () => {
     try {
-        await fastify.listen({ port: 3000 })
+        await fastify.listen({ port: 5050 })
     } catch (err) {
         fastify.log.error(err)
         process.exit(1)
